fa2dacd60f7f6c116749c41b7b82223655720fb7
Hella-Spaced <rrolle1@mail.ccsf.edu>
2016-01-21 20:49:23 -0800

    connected instagram API

--- instagram2/PhotosViewController.swift
+++ instagram2/PhotosViewController.swift
@@ -17,6 +17,28 @@ class ViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        
+        let clientId = "e05c462ebd86446ea48a5af73769b602"
+        let url = NSURL(string:"https://api.instagram.com/v1/media/popular?client_id=\(clientId)")
+        let request = NSURLRequest(URL: url!)
+        let session = NSURLSession(
+            configuration: NSURLSessionConfiguration.defaultSessionConfiguration(),
+            delegate:nil,
+            delegateQueue:NSOperationQueue.mainQueue()
+        )
+        
+        let task : NSURLSessionDataTask = session.dataTaskWithRequest(request,
+            completionHandler: { (dataOrNil, response, error) in
+                if let data = dataOrNil {
+                    if let responseDictionary = try! NSJSONSerialization.JSONObjectWithData(
+                        data, options:[]) as? NSDictionary {
+                            NSLog("response: \(responseDictionary)")
+                    }
+                }
+        });
+        task.resume()
+        
         // Do any additional setup after loading the view, typically from a nib.
     }
 
